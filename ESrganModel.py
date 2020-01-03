fc37aad956ac489f489782018fe470eb7f875058
Badaoui Ahmed Badis <32328852+Papirapi@users.noreply.github.com>
2020-01-03 15:52:54 +0100

    Add files via upload

---
+++ ESrganModel.py
@@ -0,0 +1,301 @@
+import os
+import tensorflow
+import skimage
+import numpy as np
+from tensorflow.keras.models import Sequential, Model, load_model
+from tensorflow.keras.layers import Input, Activation, Add, Concatenate, Multiply, Flatten, add
+from tensorflow.keras.layers import BatchNormalization, LeakyReLU, PReLU, Conv2D, Dense
+from tensorflow.keras.layers import UpSampling2D, Lambda, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.applications.vgg19 import preprocess_input
+from tensorflow.keras import backend as K
+from keras.callbacks import TensorBoard, ModelCheckpoint, LambdaCallback
+
+class ESRGAN():
+
+    """ Model Architecture is inspired from the Paper:
+        https://arxiv.org/pdf/1809.00219.pdf   """
+
+    def __init__(self,gen_lr=1e-4, dis_lr=1e-4,training_mode=True,loss_weights=[1e-3, 0.006]):
+
+        # Input / Output shape
+        self.channels = 3
+        self.lr_dim = 128                 # Low resolution dimension
+        self.lr_shape = (self.lr_dim, self.lr_dim , self.channels)
+        self.hr_dim = 384                 # High resolution dimesion
+        self.hr_shape = (self.hr_dim, self.hr_dim, self.channels)
+        
+        # Gan setup settings
+        self.gan_loss = 'mse'
+        self.dis_loss = 'binary_crossentropy'
+        self.gen_lr = gen_lr
+        self.dis_lr = dis_lr
+        # Build & compile the generator network
+        self.generator = self.build_generator()
+        self.compile_generator(self.generator)
+        # Learning rates
+        
+
+        self.loss_weights = loss_weights
+
+        if training_mode:
+            self.vgg = self.build_vgg()
+            self.compile_vgg(self.vgg)
+            self.discriminator = self.build_discriminator()
+            self.compile_discriminator(self.discriminator)
+            self.esrgan = self.build_esrgan()
+            self.compile_esrgan(self.esrgan)
+
+
+    def build_vgg(self):
+        """
+        Load pre-trained VGG weights from keras applications
+        Extract features to be used in loss function from last conv layer, see architecture at:
+        https://github.com/keras-team/keras/blob/master/keras/applications/vgg19.py
+        """
+
+        # Input image to extract features from
+        img = Input(shape=(self.hr_shape[0],self.hr_shape[1],3))
+
+        # Get the vgg network. Extract features from last conv layer
+        vgg = VGG19(weights="imagenet",include_top=False,input_shape=(self.hr_shape[0],self.hr_shape[1],3))
+        vgg.outputs = [vgg.layers[20].output]
+
+        # Create model and compile
+        model = Model(inputs=img, outputs=vgg(img))
+        model.trainable = False
+        return model    
+
+    def preprocess_vgg(self, x):
+        """Take a HR image [-1, 1], convert to [0, 255], then to input for VGG network"""
+        if isinstance(x, np.ndarray):
+            return preprocess_input((x+1)*127.5)
+        else:            
+            return Lambda(lambda x: preprocess_input(tensorflow.add(x, 1) * 127.5))(x)
+            
+
+    def build_generator(self, ):
+        """
+        Build the generator network according to description in the paper.
+        :return: the compiled model
+        """
+
+        def dense_block(input):
+            x1 = Conv2D(64, kernel_size=3, strides=1, padding='same')(input)
+            x1 = LeakyReLU(0.2)(x1)
+            x1 = Concatenate()([input, x1])
+
+            x2 = Conv2D(64, kernel_size=3, strides=1, padding='same')(x1)
+            x2 = LeakyReLU(0.2)(x2)
+            x2 = Concatenate()([input, x1, x2])
+
+            x3 = Conv2D(64, kernel_size=3, strides=1, padding='same')(x2)
+            x3 = LeakyReLU(0.2)(x3)
+            x3 = Concatenate()([input, x1, x2, x3])
+
+            x4 = Conv2D(64, kernel_size=3, strides=1, padding='same')(x3)
+            x4 = LeakyReLU(0.2)(x4)
+            x4 = Concatenate()([input, x1, x2, x3, x4])
+
+            x5 = Conv2D(64, kernel_size=3, strides=1, padding='same')(x4)
+            x5 = Lambda(lambda x: x * 0.2)(x5)
+            x = Add()([x5, input])
+            return x
+
+        def RRDB(input):
+            x = dense_block(input)
+            x = dense_block(x)
+            x = dense_block(x)
+            x = Lambda(lambda x: x * 0.2)(x)
+            out = Add()([x, input])
+            return out
+
+
+        def up_sample_block(model, kernel_size=3, filters=256, strides=1, size=3, skip=None):
+
+
+            if skip is not None:
+                model = Concatenate(axis=-1)([model, skip])
+
+            model = Conv2D(filters, kernel_size, strides, padding = "same", kernel_initializer='he_normal',name='upSampleConv2d_' + str(size))(model)
+            model = PReLU(shared_axes=[1, 2], name='upSamplePReLU_' + str(size))(model)
+            model = UpSampling2D(size = size)(model)
+  
+            return model
+
+        # Input low resolution image
+        lr_input = Input(shape= self.lr_shape)
+
+        # Pre-residual
+        x_start = Conv2D(64, kernel_size=3, strides=1, padding='same')(lr_input)
+        x_start = LeakyReLU(0.2)(x_start)
+
+        # Residual-in-Residual Dense Block
+        x = RRDB(x_start)
+
+        # Post-residual block
+        x = Conv2D(64, kernel_size=3, strides=1, padding='same')(x)
+        x = Lambda(lambda x: x * 0.2)(x)
+        x = Add()([x, x_start])
+
+        # Upsampling depending on factor
+        x = up_sample_block(x, size=3)
+        x = Conv2D(64, kernel_size=3, strides=1, padding='same')(x)
+        x = LeakyReLU(0.2)(x)
+        hr_output = Conv2D(3, kernel_size=3, strides=1, padding='same', activation='tanh')(x)
+
+        # Create model and compile
+        model = Model(inputs=lr_input, outputs=hr_output)
+        model.summary()
+
+        return model
+
+
+    def build_discriminator(self, filters=64):
+        """
+        Build the discriminator network according to description in the paper.
+        """
+
+        def conv2d_block(input, filters, strides=1, bn=True):
+            d = Conv2D(filters, kernel_size=3, strides=strides, padding='same')(input)
+            d = LeakyReLU(alpha=0.2)(d)
+            if bn:
+                d = BatchNormalization(momentum=0.8)(d)
+            return d
+
+        # Input high resolution image
+        img = Input(shape=self.hr_shape)
+        x = conv2d_block(img, filters, bn=False)
+        x = conv2d_block(x, filters, strides=2)
+        x = conv2d_block(x, filters * 2)
+        x = conv2d_block(x, filters * 2, strides=2)
+        x = conv2d_block(x, filters * 4)
+        x = conv2d_block(x, filters * 4, strides=2)
+        x = conv2d_block(x, filters * 8)
+        x = conv2d_block(x, filters * 8, strides=2)
+        x = Dense(filters * 16)(x)
+        x = LeakyReLU(alpha=0.2)(x)
+        x = Dropout(0.4)(x)
+        x = Dense(1)(x)
+ 
+        # Create model and compile
+        model = Model(inputs=img, outputs=x)
+
+        return model
+
+
+    def build_esrgan(self):
+        """Create the combined ESRGAN network"""
+
+        # Input LR images
+        img_lr = Input(self.lr_shape)
+
+        # Create a high resolution image from the low resolution one
+        generated_hr = self.generator(img_lr)
+        generated_features = self.vgg(
+            self.preprocess_vgg(generated_hr)
+        )
+
+        # In the combined model we only train the generator
+        self.discriminator.trainable = False
+
+        # Determine whether the generator HR images are OK
+        generated_check = self.discriminator(generated_hr)
+        
+        # Create sensible names for outputs in logs
+        generated_features = Lambda(lambda x: x, name='Content')(generated_features)
+        generated_check = Lambda(lambda x: x, name='Adversarial')(generated_check)
+
+        # Create model and compile
+        # Using binary_crossentropy with reversed label, to get proper loss, see:
+        # https://danieltakeshi.github.io/2017/03/05/understanding-generative-adversarial-networks/
+        model = Model(inputs=img_lr, outputs=[generated_check, generated_features])        
+        return model
+
+
+    def compile_vgg(self, model):
+        """Compile the generator with appropriate optimizer"""
+        model.compile(
+            loss='mse',
+            optimizer=Adam(self.gen_lr, 0.9),
+            metrics=['accuracy']
+        )
+
+
+    def compile_generator(self, model):
+        """Compile the generator with appropriate optimizer"""
+        model.compile(
+            loss=self.gan_loss,
+            optimizer=Adam(self.gen_lr, 0.9),
+            metrics=['mse', self.cPSNR]
+        )
+
+
+    def compile_discriminator(self, model):
+        """Compile the generator with appropriate optimizer"""
+        model.compile(
+            loss=self.dis_loss,
+            optimizer=Adam(self.dis_lr, 0.9),
+            metrics=['accuracy']
+        )
+
+
+    def compile_esrgan(self, model):
+        """Compile the GAN with appropriate optimizer"""
+        model.compile(
+            loss=[self.dis_loss, self.gan_loss],
+            loss_weights=self.loss_weights,
+            optimizer=Adam(self.gen_lr, 0.9)
+        )
+
+
+    def cMSE(self, y_true, y_pred):
+        # apply the quality mask
+        obs = tensorflow.equal(y_true, 0.05)
+        clr = tensorflow.math.logical_not(obs)
+        _hr = tensorflow.boolean_mask(y_true, clr )
+        _sr = tensorflow.boolean_mask(y_pred, clr )
+
+        # calculate the bias in brightness b
+        pixel_diff = _hr - _sr
+        b = K.mean(pixel_diff)
+
+        # calculate the corrected clear mean-square error
+        pixel_diff -= b
+        cMse = K.mean(pixel_diff * pixel_diff)
+
+        return cMse 
+
+
+    def cPSNR(self, y_true, y_pred):
+        """
+        PSNR is Peek Signal to Noise Ratio, see https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
+        The equation is:
+        PSNR = 20 * log10(MAX_I) - 10 * log10(MSE)
+        
+        Since input is scaled from -1 to 1, MAX_I = 1, and thus 20 * log10(1) = 0. Only the last part of the equation is therefore neccesary.
+        """
+        return -10.0 * K.log(self.cMSE(y_true,y_pred)) / K.log(10.0)
+
+
+    def save_weights(self, filepath, e=None):
+        """Save the generator and discriminator networks"""
+        self.generator.save_weights("{}_generator_X_epoch{}.h5".format(filepath,  e))
+        self.discriminator.save_weights("{}_discriminator_X_epoch{}.h5".format(filepath, e))    
+
+
+    def load_weights(self, generator_weights=None, discriminator_weights=None, **kwargs):
+        """Load the generator and discriminator networks"""
+
+        if generator_weights:
+            self.generator.load_weights(generator_weights, **kwargs)
+        if discriminator_weights:
+            self.discriminator.load_weights(discriminator_weights, **kwargs)
+
+
+    def save_model(self, filepath, e=None):
+        """Save the generator and discriminator model"""
+       
+        self.generator.save("{}_generator_model_X_epoch{}.h5".format(filepath,  e))
+        #self.discriminator.save("{}_discriminator_model_X_epoch{}.h5".format(filepath, e))  
---
+++ train.py
@@ -0,0 +1,204 @@
+import sys
+import os
+import numpy as np
+import skimage
+import matplotlib.pyplot as plt
+import time
+import json
+import random
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from ESrganModel import ESRGAN
+from keras.utils.data_utils import OrderedEnqueuer
+
+
+#To stop showing the Tensorflow logs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+####################################################
+################ Configure Model ###################
+####################################################
+#Loading the Data
+load_imgs_hr = np.load('/content/Proba-V/training_set/HR_train.npy')
+load_imgs_lr = np.load('/content/Proba-V/training_set/LR_train.npy')
+
+def load_data(batch_size=1):
+
+    batch_images = np.random.choice(load_imgs_hr.shape[0], size=batch_size)
+    imgs_hr = load_imgs_hr[batch_images]
+    imgs_lr = load_imgs_lr[batch_images]
+    return imgs_hr, imgs_lr
+
+
+log_weight_path= '/content/Proba-V/output/'
+dataname='train_genz'
+# instantiate modules
+Esrgan = ESRGAN()
+
+
+######################################################################
+################ Vizualization And cPSNR computing ##################
+#####################################################################
+
+
+def eval_generated_images(epoch, generator, iteration,  examples=1 , dim=(1, 3),
+                         save=True , plot=True):
+
+    imgs_hr, imgs_lr = load_data(examples)
+    gen_img = generator.predict(imgs_lr)
+    imgs_hr = imgs_hr.astype(np.float32)
+    cPSNR = Esrgan.cPSNR(imgs_hr, gen_img)
+    cPSNR = cPSNR.eval(session = tf.compat.v1.Session())
+    if plot:
+        print('Calculated cPSNR: ', cPSNR)
+        fig = plt.figure(figsize=(15, 5))
+
+        plt.subplot(dim[0], dim[1], 1)
+        plt.imshow(np.squeeze(imgs_lr[0]), interpolation='nearest')
+        plt.axis('off')
+
+        plt.subplot(dim[0], dim[1], 2)
+        plt.imshow(np.squeeze(gen_img[0]), interpolation='nearest')
+        plt.axis('off')
+
+        plt.subplot(dim[0], dim[1], 3)
+        plt.imshow(np.squeeze(imgs_hr[0]), interpolation='nearest')
+        plt.axis('off')
+
+        plt.tight_layout()
+        if save:
+            fig.savefig("output/predict_{}_{}.png".format(epoch+1, iteration+1))
+            plt.close(fig)
+
+    return cPSNR
+###################################################
+################ Training Esrgan  ##################
+###################################################
+
+def train(epochs=1000, batch_size=10, sample_interval=200):
+
+    data_len = len(load_imgs_hr) # number of scenes in the dataset 1160
+    disciminator_output_shape = (batch_size,24,24,1)
+
+    # VALID / FAKE targets for discriminator
+    real = np.ones(disciminator_output_shape)
+    fake = np.zeros(disciminator_output_shape)
+    
+
+    #Uncomment the next statement if you want to load from a given trained weights
+    Esrgan.load_weights('/content/Proba-V/weights3/train_genz_generator_X_epoch2.h5', '/content/Proba-V/weights3/train_genz_discriminator_X_epoch2.h5')
+    print_losses = {"G": [], "D": [], "cPSNR" : []}
+    losses_per_epoch = {"G": [], "D": [], "cPSNR" : []}
+    for epoch in range(epochs):
+        # with open('output/epoch_{}.csv'.format(epoch), 'w') as csvfile:
+        #     filewriter = csv.writer(csvfile, delimiter=',')
+        #     filewriter.writerow(['Gen_loss1','Gen_loss2','Gen_loss3','Disc_loss1','Disc_loss2','cPSNR'])
+    # Loop through epochs / iterations
+        for iteration in range(data_len//batch_size):
+
+            print('epoch: '+ str(epoch) +' iteration: ' + str(iteration))
+ 
+            # Train discriminator   
+            imgs_hr, imgs_lr = load_data(batch_size)
+            generated_hr = Esrgan.generator.predict(imgs_lr)
+            real_loss = Esrgan.discriminator.train_on_batch(imgs_hr, real)
+            fake_loss = Esrgan.discriminator.train_on_batch(generated_hr, fake)
+            discriminator_loss = 0.5 * np.add(real_loss, fake_loss)
+            cal_cPSNR = eval_generated_images(epoch, Esrgan.generator, iteration, examples=1 , dim=(1, 3),save=True , plot=False)
+            # Train generator
+            features_hr = Esrgan.vgg.predict(Esrgan.preprocess_vgg(imgs_hr))
+            generator_loss = Esrgan.esrgan.train_on_batch(imgs_lr, [real, features_hr])            
+
+            # Save losses            
+            # with open('output/epoch_{}.csv'.format(epoch), 'a') as csvfile:
+            #     filewriter = csv.writer(csvfile, delimiter=',')
+            #     filewriter.writerow([generator_loss[0],generator_loss[1],generator_loss[2],discriminator_loss[0],discriminator_loss[1],cal_cPSNR])
+            print_losses['G'].append(generator_loss)
+            print_losses['D'].append(discriminator_loss)
+            print_losses['cPSNR'].append(cal_cPSNR)
+
+            # Save the network weights
+            # Save the network losses
+            print(">> Saving the network weights")
+            Esrgan.save_weights(os.path.join(log_weight_path, dataname), epoch)
+            # Plot gen_img every sample interval
+            if iteration == 0 or iteration % sample_interval == 0:
+                eval_generated_images(epoch, Esrgan.generator, iteration)
+                
+        # Save the network model
+        print(">> Saving the network model")
+        Esrgan.save_model(os.path.join(log_weight_path, dataname), epoch)
+        # Show the progress
+         
+        g_avg_loss = np.array(print_losses['G']).mean(axis=0)
+        d_avg_loss = np.array(print_losses['D']).mean(axis=0)
+        cPSNR_avg = np.array(print_losses['cPSNR']).mean(axis=0)
+        losses_per_epoch['G'].append(g_avg_loss.tolist())
+        losses_per_epoch['D'].append(d_avg_loss.tolist())
+        losses_per_epoch['cPSNR'].append(float(cPSNR_avg))
+
+        print("\nEpoch {}/{} \n>> Generator/GAN: {}\n>> Discriminator: {}".format(
+            epoch, epochs,
+            ", ".join(["{}={:.4f}".format(k, v) for k, v in zip(Esrgan.esrgan.metrics_names, g_avg_loss)]),
+            ", ".join(["{}={:.4f}".format(k, v) for k, v in zip(Esrgan.discriminator.metrics_names, d_avg_loss)])
+        ))
+        
+        # Save cPSNR value and Gen & Disc losses
+        with open('losses_{}.json'.format(epoch), 'w') as fp:
+            json.dump(losses_per_epoch, fp)
+
+        del print_losses
+        del losses_per_epoch
+def test(batch_size = 4):
+    gen_model = load_model('/content/Proba-V/models/train_genz_generator_model_X_epoch2.h5', custom_objects = {'cPSNR' : Esrgan.cPSNR})
+    load_imgs_lr = np.load('/content/Proba-V/LR_test.npy')
+    data_len = len(load_imgs_lr)
+    limit = 0
+    for iteration in range(data_len//batch_size):
+        
+        imgs_lr = load_imgs_lr[limit:limit+batch_size]
+        limit = iteration+batch_size
+        generated_sr = gen_model.predict(imgs_lr)
+        for i in range(len(imgs_lr)):
+
+            fig = plt.figure(figsize=(15, 5))
+            plt.subplot(1, 2, 1)
+            plt.imshow(np.squeeze(imgs_lr[i]), interpolation='nearest')
+            plt.axis('off')
+
+            plt.subplot(1, 2, 2)
+            plt.imshow(np.squeeze(generated_sr[i]), interpolation='nearest')
+            plt.axis('off')        
+            plt.tight_layout()
+            fig.savefig("predictions/predict_{}_{}.png".format(i+1, iteration+1))
+            plt.close(fig)
+
+if __name__ == '__main__':
+
+    if len(sys.argv)==1:
+        print('Please insert train or test argument')
+        sys.exit(0)
+    if sys.argv[1] == 'train':
+        epochs = 3
+        train(epochs=epochs, batch_size=4, sample_interval=90)
+        print("#"*100)
+        print("#"*36," {} Epochs Completed! ".format(epochs),"#"*36)
+        print("#"*100)
+    elif sys.argv[1] == 'test':
+        test(batch_size = 1)
+        print("#"*100)
+        print(" "*6,"#"*30,"Predictions Completed! ","#"*30)
+        print("#"*100)
+    else:
+        print('You can use only train or test options')
+
+
+    # ------------------------------------------------------------------------
+    #  These parameters are set to train for 1000 epochs, with 16GB of GPU
+    #  VRAM available on Colab and 25GB RAM CPU.
+    #  Due to the fact that Colab crashs often,I have divided the training
+    #  into mini-training where epochs = 10 
+    # ------------------------------------------------------------------------
+
+
